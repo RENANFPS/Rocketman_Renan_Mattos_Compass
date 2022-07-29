d6db5be67d5a523fe0f897305f7a2d366acfa638
Renan Mattos <mattosrocharenan@gmail.com>
2022-07-29 16:23:50 -0300

    Mini Projeto 5

---
+++ miniProjeto5.js
@@ -0,0 +1,12 @@
+// Receber uma quantidade de valores para avaliar
+// funcao exibe se cada valor e par ou impar
+
+exibirTipo(20);
+function exibirTipo(limite){
+    for (let i =0; i<=limite; i++) {
+        if (i % 2 === 0)
+            console.log(i,'PAR')
+            else
+            console.log(i,'IMPAR')
+    }
+}
