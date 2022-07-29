2cd73cba99d1ec2cd72d1bbe258fd01576a43ca4
Renan Mattos <mattosrocharenan@gmail.com>
2022-07-29 12:04:31 -0300

    Mini projeto 2

---
+++ miniProjeto2.js
@@ -0,0 +1,11 @@
+// Escreva uma funcao que usa 2 numeros e retorna o maior entre eles
+
+let valorMaior = max(10,15);
+console.log(valorMaior);
+
+
+function max(numero1,numero2){
+    return numero1 > numero2 ? numero1 : numero2;
+}
+
+
