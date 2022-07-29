de4c13b12aaa751794ae4ffee833662aa494bc4d
Renan Mattos <mattosrocharenan@gmail.com>
2022-07-29 15:42:13 -0300

    Mini projeto 4

---
+++ miniProjeto4.js
@@ -0,0 +1,22 @@
+// Velocidade maxima de ate 70
+// a cada 5km acima do limite voce ganha 1 ponto
+//Math.Floor()
+// caso pontos maior que 12 -> "Carteira Suspensa"
+
+verificarVelocidade(70);
+
+
+function verificarVelocidade(velocidade){
+    const velocidadeMaxima = 70;
+    const kmPorPonto = 5;
+    if (velocidade <= velocidadeMaxima)
+    console.log('ok');
+    else {
+        const pontos = Math.floor ((velocidade - velocidadeMaxima) / kmPorPonto);
+        if ( pontos >= 12 )
+            console.log('Carteira supensa');
+        else
+            console.log('Pontos' , pontos);
+
+    }
+}
