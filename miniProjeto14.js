4599ad027cc11c15c88643e9f5f857780dcb3f29
Renan Mattos <mattosrocharenan@gmail.com>
2022-08-01 16:16:27 -0300

    Mini Projeto 14

---
+++ miniProjeto14.js
@@ -0,0 +1,27 @@
+//Faixa de preco
+
+//Primeira Opcao
+let faixas = [
+    {tooltip: 'ate R$700' , minimo: 0, maximo:700},
+    {tooltip:'de R$700 a R$1000', minimo: 700, maximo: 1000},
+    {tooltip: 'R$1000 ou mais', minimo:1000, maximo: 999999}
+];
+
+// Segunda opcao ( Factory function)
+
+function criaFaixaPreco(tooltip,minimo,maximo) {
+    return {
+        tooltip,
+        minimo,
+        maximo
+    }
+}
+
+let faixas2 = [
+    criaFaixaPreco('a',1,100),
+    criaFaixaPreco('b',100,1000),
+    criaFaixaPreco('c',1000,10000),
+
+]
+console.log(faixas);
+console.log(faixas2);
