d83b48b6645e923ddcd197ec971e8ef26eb545be
Renan Mattos <mattosrocharenan@gmail.com>
2022-08-01 11:36:12 -0300

    Mini Projeto 11

---
+++ miniProjeto11.js
@@ -0,0 +1,19 @@
+// Javascript - Objeto Endereco
+
+//Criar um objeto endereco que contem
+//Rua; Cidade; CEP; exibirEndereco(endereco)
+
+let endereco = {
+    Rua: 'Av suburbana',
+    Cidade: 'Rio de Janeiro',
+    CEP: '500000'
+};
+
+function exibirEndereco(endereco) {
+    for(let chave in endereco)
+        console.log(chave,endereco[chave]);
+}
+ 
+exibirEndereco(endereco);
+
+
