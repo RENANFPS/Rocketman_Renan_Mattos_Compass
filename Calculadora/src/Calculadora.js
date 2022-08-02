c893edfb7e15f626e6d6f48b188161ed64a15ac4
Renan Mattos <mattosrocharenan@gmail.com>
2022-08-02 18:32:21 -0300

    Calculadora

---
+++ Calculadora/src/Calculadora.js
@@ -0,0 +1,23 @@
+export default class Calculadora {
+
+    static soma(a,b) {
+        return a + b
+    }
+
+    static sub(a,b) {
+        return a - b
+    }
+
+    static mult(a,b) {
+        return a * b
+    }
+
+    static div(a,b) {
+        return a / b
+    }
+}
+
+let a = prompt('Olá, qual o primeiro número?');
+let b = prompt('e qual o segundo número?');
+
+alert(resultado);
